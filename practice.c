6bef541c6fa588e2d74175c9700eae9e0de03718
Max <flemingm@whitman.edu>
2025-09-29 13:13:05 -0700

    implemented first question

---
+++ practice.c
@@ -0,0 +1,14 @@
+
+
+int main(){
+   int arr1[3];
+   int arr2[3];
+   int dotProduct = 0; 
+
+   printf("Enter 3 nums for vector 1");
+   scanf("%d  %d  %d", &arr1[0], &arr1[1]. &arr1[2]);
+    printf("3 more for the second vector");
+
+   scanf("%d  %d  %d", &arr2[0], &arr2[1]. &arr2[2]);
+
+}
